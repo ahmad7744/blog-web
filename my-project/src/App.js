653a5916e0cc411ec9b6693a9542b6d1723810af
ahmad7744 <ahmedaslam.alphasquad@gmail.com>
2023-07-07 22:10:02 +0500

    first commit

---
+++ my-project/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      {/* Hero section start */}
+      <div class="w-full h-screen bg-white ">
+        <div>
+        <p class="text-center mt-32 text-[48px] font-semibold text-gray-800">A UX Case Study on Creating a<p></p> Studious Environment for Students</p>
+        <p class="opacity-90 text-center text-gray-800 text-[16px] font-medium mt-[16px] ">Andrew Jonson<span class="text-black text-[16px] font-medium"> Posted on 27th January 2021</span></p>
+        <img src="" />
+        
+        </div>
+        
+
+
+      </div>
+    
+    </div>
+  )
+}
+
+export default App
